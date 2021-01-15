e98e86dd05446ca56bb2bf6114bbbf9a353e9629
Andrew Han <junmenghan@gmail.com>
2021-01-15 02:41:31 +0000

    Added time conversion

--- index.js
+++ index.js
@@ -1,45 +1,11 @@
 const { nextISSTimesForMyLocation } = require('./iss');
 
-// let fetchedIp = "";
-
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("Error Fetching IP" , error);
-//     return;
-//   }
-//   console.log('IP Successfully Fetched:' , ip);
-//   fetchedIp = ip;
-// });
-
-// fetchCoordsByIP(fetchedIp, (error, data) => {
-//   if (error) {
-//     console.log("Error fetching co-ords", error);
-//     return;
-//   }
-//   console.log("Co-ords successfully fetched: ", data);
-// })
-
-// fetchISSFlyOverTimes({latitude: 49.31, longitude: -122.85}, (error, data) => {
-//   if (error) {
-//     console.log("Error fetching flyover times", error);
-//     return;
-//   }
-//   console.log("Flyover times fetched: ", data);
-// })
-
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("Error: ", error);
   }
-  console.log(passTimes);
+  const datetime = new Date(0);
+  datetime.setUTCSeconds(passTimes[0].risetime);
+  const duration = passTimes[0].duration;
+  console.log(`Next pass at ${datetime} for ${duration} seconds!`);
 });
-
-
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned IP:' , ip);
-// });
